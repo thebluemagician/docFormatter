36148ca4d333bf084446f28fc9c40897d0fe5584
Md Adil <adil.md70@gmail.com>
2017-01-18 22:58:27 +0530

    N

---
+++ app.js
@@ -0,0 +1,17 @@
+const editor = document.querySelector('.editor');
+const controlBtns = [...document.querySelectorAll('.controls.btn')];
+const fontSize = document.querySelector('#font-size');
+const fontFamily = document.querySelector('#font-family');
+
+controlBtns[0].addEventListener('click', () => document.execCommand('bold', false, null) );
+controlBtns[1].addEventListener('click', () => document.execCommand('italic', false, null) );
+controlBtns[2].addEventListener('click', () => document.execCommand('underline', false, null) );
+controlBtns[3].addEventListener('click', () => document.execCommand('strikethrough', false, null) );
+
+editor.addEventListener('keydown', function() {
+	document.execCommand('fontSize', false, fontSize.value);
+});
+
+fontFamily.addEventListener('input', function() {
+	document.execCommand('fontName', false, this.value);
+});
