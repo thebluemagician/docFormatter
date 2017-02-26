395d6b0e782f95adf3ccd9871ec0e99e317f693f
Soumyajit Pathak <s.pathak5995@gmail.com>
2017-02-26 20:06:17 +0530

    basic data update

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-// const editor = document.querySelector('.editor');
-// const controlBtns = [...document.querySelectorAll('.controls.btn')];
-// const fontSize = document.querySelector('#font-size');
-// const fontFamily = document.querySelector('#font-family');
+const currentData = window.localStorage.docFormatter ? JSON.parse(window.localStorage.docFormatter) : { currentNode: 1, 'root': { title: 'The Title', data: ''}}
 
-// controlBtns[0].addEventListener('click', () => document.execCommand('bold', false, null) );
-// controlBtns[1].addEventListener('click', () => document.execCommand('italic', false, null) );
-// controlBtns[2].addEventListener('click', () => document.execCommand('underline', false, null) );
-// controlBtns[3].addEventListener('click', () => document.execCommand('strikethrough', false, null) );
+function pushToLocalStorage() {
+	window.localStorage.setItem('docFormatter', JSON.stringify(currentData));
+}
 
-// editor.addEventListener('keydown', function() {
-// 	document.execCommand('fontSize', false, `${fontSize.value}`);
-// });
-
-// fontFamily.addEventListener('input', function() {
-// 	document.execCommand('fontName', false, this.value);
-// });
+// EDITOR
 
 const toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
@@ -37,3 +27,132 @@ const quill = new Quill('#editor', {
 		toolbar: toolbarOptions
 	}
 });
+
+// TREE
+
+const tree = document.querySelector('.tree-wrapper');
+
+function parentIcon(parent) {
+	const icon = parent.querySelector('.expand');
+	if (parent.querySelector('ul')) {
+		icon.classList.remove('fa-file');
+		icon.classList.add('fa-folder');
+	} else {
+		icon.classList.remove('fa-folder');
+		icon.classList.add('fa-file');
+	}
+}
+
+function* nodeGenerator() {
+	while (true) {
+		yield `node${currentData.currentNode++}`;
+	}
+}
+
+const getNodeNo = nodeGenerator();
+
+tree.addEventListener('click', e => {
+	const target = e.srcElement;
+	const parent = target.parentElement;
+	if ([...target.classList].includes('fa-plus')) {
+		const ul = parent.querySelector('ul') || function(){
+			let result = document.createElement('ul');
+			parent.appendChild(result);
+			return result;
+		}();
+		const collapse = document.createElement('i');
+		collapse.classList.add('fa');
+		collapse.classList.add('fa-file');
+		collapse.classList.add('expand');
+		collapse.ariaHidden = 'true';
+		const li = document.createElement('li');
+		const button = document.createElement('i');
+		button.classList.add('fa');
+		button.classList.add('fa-plus');
+		button.ariaHidden = 'true';
+		const link = document.createElement('a');
+
+		const node = getNodeNo.next().value;
+		link.href = `#/${node}`;
+		link.textContent = `Untitled`;
+		link.setAttribute('data-link', node);
+		
+		currentData[node] = {};
+		currentData[node]['data'] = 'Start writing...';
+		currentData[node]['title'] = 'Untitled';
+
+		pushToLocalStorage();
+		
+		li.classList.add('node');
+		li.appendChild(collapse);
+		li.appendChild(link);
+		li.appendChild(button);
+		ul.appendChild(li);
+		parentIcon(parent);
+	}
+});
+
+// DATA STORAGE AND LINKAGE
+
+const titleName = document.querySelector('.title-name');
+const remove = document.querySelector('.remove');
+const editor = document.querySelector('.ql-editor');
+const root = document.querySelector('.root');
+
+
+function getCurrentAddress() {
+	return String(window.location.hash.substring(2, window.location.hash.length));
+}
+
+window.addEventListener('hashchange', function(e) {
+	const location = getCurrentAddress();
+	const activeLink = root.querySelector(`a[data-link=${location}]`);
+	[...root.querySelectorAll('a')].forEach( a => {
+		if (a.getAttribute('data-link') === location) {
+			a.classList.add('active');
+		} else {
+			a.classList.remove('active');
+		}
+	});
+	editor.innerHTML = currentData[location]['data'];
+	titleName.value = currentData[location]['title'];
+});
+
+function storeData() {
+	const data = String(this.innerHTML);
+	const node = getCurrentAddress();
+	currentData[node]['data'] = data;
+	pushToLocalStorage();
+}
+
+function storeTitle() {
+	const node = getCurrentAddress();
+	const value = this.value.trim();
+	currentData[node]['title'] = value;
+	pushToLocalStorage();
+	const element = root.querySelector(`a[data-link=${node}]`);
+	element.textContent = value;
+}
+
+function removeNode() {
+	const node = getCurrentAddress();
+	if (node === 'root') {
+		alert('Cannot delete root');
+		return;
+	}
+	delete currentData[node];
+	pushToLocalStorage();
+	const element = root.querySelector(`a[data-link=${node}]`);
+	const ul = element.parentElement.parentElement;
+	const li = element.parentElement;
+	ul.removeChild(li);
+}
+
+editor.addEventListener('input', storeData);
+titleName.addEventListener('input', storeTitle);
+remove.addEventListener('click', removeNode);
+
+document.querySelector('.format').addEventListener('click', () => {
+	window.localStorage.removeItem('docFormatter');
+	window.location.reload();
+})
